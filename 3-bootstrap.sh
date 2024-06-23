36bdc677340da2ed84e7812082098481ac834ff5
affixpin <dmytro.pintak@gmail.com>
2024-06-23 18:09:41 +0100

    alacritty

--- 3-bootstrap.sh
+++ 3-bootstrap.sh
@@ -8,6 +8,6 @@ sv start ntpd
 
 basestrap /mnt base base-devel runit elogind-runit
 basestrap /mnt linux-lts linux-firmware intel-ucode
-basestrap /mnt dhcpcd dhcpcd-runit wpa_supplicant wpa_supplicant-runit neovim efibootmgr git mkinitcpio cryptsetup cryptsetup-runit neofetch man-db mesa sudo sway
+basestrap /mnt dhcpcd dhcpcd-runit wpa_supplicant wpa_supplicant-runit neovim efibootmgr git mkinitcpio cryptsetup cryptsetup-runit neofetch man-db mesa sudo sway alacritty
 
 fstabgen -U /mnt >> /mnt/etc/fstab
